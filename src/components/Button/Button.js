e773c30b644b94cc5dc0518d704e09f1907b12c0
Alym <satybaldyevalym4@gmail.com>
2021-12-02 18:04:53 +0600

    Added folder components

---
+++ src/components/Button/Button.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Button.css"
+const Button = () =>{
+    return(
+        <div className="Button">
+            <button>click</button>
+        </div>
+    )
+}
+export default Button;
